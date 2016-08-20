810bc89e629edb16a85c7746e75d7937a08d6e14
bsstahl <barry@bsstahl.com>
2016-08-20 07:58:21 -0700

    Client_Pause_Should.ReduceTheNumberOfActiveProcessors

--- LiquidVictor/test/LV.Publication.Management.Test/Client_Pause_Should.cs
+++ LiquidVictor/test/LV.Publication.Management.Test/Client_Pause_Should.cs
@@ -38,5 +38,30 @@ namespace LV.Publication.Management.Test
             }
         }
 
+        [Fact]
+        public static void ReduceTheNumberOfActiveProcessors()
+        {
+            int startCount = 10.GetRandom(5);
+            int expectedCount = startCount - 1;
+            var configRepo = new Mocks.MockConfigRepository(startCount);
+            var factory = new Mocks.MockSourceProcessorFactory();
+            var target = (null as Client).Create(configRepo, factory);
+
+            var timeoutTask = Task.Delay(100);
+            target.Start();
+
+            var processor = factory.GetRandomProcessor();
+            System.Diagnostics.Debug.Assert(processor.IsActive);
+
+            try
+            {
+                target.Pause(processor.Id);
+                Assert.Equal(expectedCount, target.ActiveProcessorCount);
+            }
+            finally
+            {
+                target.Stop();
+            }
+        }
     }
 }
