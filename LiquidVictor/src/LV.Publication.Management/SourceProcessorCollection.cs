388cc9519e95ec06f0c5ee0325055d0c6c66b8dd
bsstahl <barry@bsstahl.com>
2016-08-20 08:18:03 -0700

    Changed KillProcessorsPastTimeout so it no longer removes stopped processors from the collection.  It already skips them, no need to remove them and create more potential thread conflicts.

--- LiquidVictor/src/LV.Publication.Management/SourceProcessorCollection.cs
+++ LiquidVictor/src/LV.Publication.Management/SourceProcessorCollection.cs
@@ -84,17 +84,16 @@ namespace LV.Publication.Management
                     {
                         processor.Stop();
                         trouble.Add(processor);
+                        _logger.LogWarning("Processor {0} stopped due to no activity since {1}", processor.Id, processor.LastAttempt);
                     }
                 }
 
                 foreach (var processor in trouble)
                 {
-                    _logger.LogWarning("Killing processor {0} due to no activity since {1}", processor.Id, processor.LastAttempt);
-                    this.Remove(processor);
                     var source = _sourceProcessorFactory.Create(processor.Config);
                     this.Add(source);
                     source.Start();
-                    _logger.LogInformation("Processor {0} started", source.Id);
+                    _logger.LogInformation("Processor {0} started to replace processor {1}", source.Id, processor.Id);
                 }
 
                 _logger.LogInformation("Process timeout check completed");
