656f81f1dfdfb351e48eb010a0a2a2bacbd7614d
Barry Stahl <barry@bsstahl.com>
2025-06-10 06:07:31 -0700

    Clarify Workshop Format Details

--- src/LiquidVictor/Enumerations/Format.cs
+++ src/LiquidVictor/Enumerations/Format.cs
@@ -7,7 +7,7 @@ public enum Format
     Session = 2,        // Roughly 1 hour (45 min - 75 min)
     Talk = 3,           // Roughly 1.5 hours (75 min - 2 hours)
     ShortWorkshop = 4,  // 2 hours up to 4 hours or so
-    Workshop = 5,       // More than 4 hours up to 1 day 
+    Workshop = 5,       // 4 hours up to 1 day 
     LongWorkshop = 6,   // Muliple days
     Keynote = 7,        // Keynote addresses
     Reference = 8       // Website or other reference documentation
